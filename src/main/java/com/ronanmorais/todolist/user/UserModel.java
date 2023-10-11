90d245cdeaa654810ad03fbfc99d6a2066b4455c
Ronan <ronan86@gmail.com>
2023-10-11 14:34:52 +1030

    first commit

---
+++ src/main/java/com/ronanmorais/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package com.ronanmorais.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+  private String username;
+  private String name;
+  private String password;
+}
